abb139b42816b4fa38e7af7da82661d6c29f7f65
VinGuler <vinguler@gmail.com>
2022-07-30 15:56:37 +0300

    added count_relations to graphs.js and small refactore

--- Algorithms/graphs.js
+++ Algorithms/graphs.js
@@ -10,22 +10,53 @@
  */
 const graph = {
   A: ['B', 'C'],
-  B: ['D', 'E'],
-  C: ['F'],
-  D: [],
-  E: ['F'],
+  B: ['A', 'E'],
+  C: ['A'],
+  D: ['E'],
+  E: ['B', 'D'],
   F: []
 }
 
+// Counts all the relation groups in the graph
+function count_relations(graph) {
+  let group_count = 0
+  const groups = {}
+
+  const is_node_in_existing_group = (graph, node) => {
+    const related_nodes = dfs(graph, node)
+    // If any of the related nodes is a group key -> this node is included there
+    for (const related_node of related_nodes) {
+      if (groups[related_node]) {
+        return true
+      }
+    }
+    return false
+  }
+
+  for (const node in graph) {
+    if (graph[node].length === 0) {
+      group_count++
+      continue
+    }
+    if (!is_node_in_existing_group(graph, node)) {
+      group_count++
+      groups[node] = graph[node]
+    }
+  }
+
+  return group_count
+}
+
+// Returns all the nodes related to the node_name
 // breadth-first-search
-function bfs(node_name) {
-  const result = []
+function bfs(graph, node_name) {
+  const all_related_nodes = []
   const queue = [node_name]
   const visited = {}
   while (queue.length) {
     const current = queue.shift()
     if (!visited[current]) {
-      result.push(current)
+      all_related_nodes.push(current)
     }
     visited[current] = true
     if (graph[current] && graph[current].length) {
@@ -33,26 +64,27 @@ function bfs(node_name) {
         if (!visited[node]) {
           queue.push(node)
           visited[node] = true
-          result.push(node)
+          all_related_nodes.push(node)
         }
       }
     }
   }
-  return result
+  return all_related_nodes
 }
 
+// Returns all the nodes related to the node_name
 // depth-first-search
-function dfs(node_name, visited = {}) {
-  const result = []
+function dfs(graph, node_name, visited = {}) {
+  const all_related_nodes = []
   if (!visited[node_name]) {
-    result.push(node_name)
+    all_related_nodes.push(node_name)
     visited[node_name] = true
     for (const node of graph[node_name]) {
-      result.push(...dfs(node, visited))
+      all_related_nodes.push(...dfs(graph, node, visited))
     }
   }
-  return result
+  return all_related_nodes
 }
 
-const res = dfs('A')
+const res = count_relations(graph)
 console.log('res', res)
